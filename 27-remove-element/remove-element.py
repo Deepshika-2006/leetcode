a4cd4c4a903ff1deb74c55bf1616cf00647483bd
Garlapati Deepshika <deepshikagarlapati@gmail.com>
2026-07-30 16:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.59%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,14 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        v=[]
-        n=[]
+        ans = []
         for i in nums:
-            if i==val:
-                v.append(i)
-            else:
-                n.append(i)
-                
-        # Copy the correct elements back into the original nums array
-        nums[:len(n)] = n
-        
-        return len(n)
+            if i != val:
+                ans.append(i)
+        nums[:] = ans
+        return len(ans)
